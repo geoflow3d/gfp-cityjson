af1de44d466deede5a03002c4c4e121b769aa0a9
Ravi Peters <r.y.peters@tudelft.nl>
2021-07-21 22:54:50 +0200

    implement geometry writing

--- nodes.hpp
+++ nodes.hpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iomanip>
 #include <geoflow/geoflow.hpp>
 #include <nlohmann/json.hpp>
 
@@ -81,6 +82,136 @@ namespace geoflow::nodes::cityjson {
             }
           }
         }
+      }
+    }
+  };
+
+  class CityJSONWriterNode : public Node {
+
+    // parameter variables
+    std::string filepath_;
+
+    public:
+    using Node::Node;
+    
+    void init() {
+      // declare ouput terminals
+      add_vector_input("multisolids", typeid(std::unordered_map<int, Mesh>));
+      // add_poly_input("attributes", {typeid(bool), typeid(int), typeid(float), typeid(std::string)});
+
+      // declare parameters
+      add_param(ParamPath(filepath_, "filepath", "File path"));
+      // add_param(ParamInt(extract_lod_, "extract_lod", "precision"));
+    }
+
+    void add_vertices(std::map<arr3f, size_t>& vertex_map, std::vector<arr3f>& vertex_vec, std::set<arr3f>& vertex_set, const Mesh& mesh) {
+      size_t v_cntr = vertex_vec.size();
+      for (auto &face : mesh.get_polygons())
+      {
+        for (auto &vertex : face)
+        {
+          auto [it, did_insert] = vertex_set.insert(vertex);
+          if (did_insert)
+          {
+            vertex_map[vertex] = v_cntr++;
+            vertex_vec.push_back(vertex);
+          }
+        }
+      }
+    }
+
+    void process() {
+      // inputs
+      auto& multisolids = vector_input("multisolids");
+
+      nlohmann::json outputJSON;
+
+      outputJSON["type"] = "CityJSON";
+      outputJSON["version"] = "1.0";
+      outputJSON["CityObjects"] = nlohmann::json::object();
+
+      std::map<arr3f, size_t> vertex_map;
+      std::vector<arr3f> vertex_vec;
+      std::set<arr3f> vertex_set;
+      size_t id_cntr = 0;
+
+      for (size_t i=0; i<multisolids.size(); ++i) {
+        auto& ms = multisolids.get<std::unordered_map<int, Mesh>>(i);
+
+        auto building = nlohmann::json::object();
+        auto b_id = std::to_string(++id_cntr);
+        building["type"] = "Building";
+        building["geometry"] = nlohmann::json::array();
+        // building["attributes"]
+        // building["children"]
+
+        std::vector<std::string> buildingPartIds;
+
+        for ( auto& [sid, solid] : ms ) {
+          auto buildingPart = nlohmann::json::object();
+          auto bp_id = std::to_string(++id_cntr);
+          buildingPartIds.push_back(bp_id);
+          buildingPart["type"] = "BuildingPart";
+          buildingPart["parents"] = {b_id};
+          add_vertices(vertex_map, vertex_vec, vertex_set, solid);
+          auto geometry = nlohmann::json::object();
+          geometry["type"] = "Solid";
+          geometry["lod"] = 2.2;
+          std::vector<std::vector<std::vector<size_t>>> exterior_shell;
+
+          for (auto &face : solid.get_polygons())
+          {
+            std::vector<std::vector<size_t>> jface;
+            std::vector<size_t> exterior_ring;
+            for (auto &vertex : face) {
+              exterior_ring.push_back(vertex_map[vertex]);
+            }
+            jface.emplace_back(std::move(exterior_ring));
+            for (auto &iring : face.interior_rings()) {
+              std::vector<size_t> interior_ring;
+              for (auto &vertex : iring) {
+                interior_ring.push_back(vertex_map[vertex]);
+              }
+              jface.emplace_back(std::move(interior_ring));
+            }
+            exterior_shell.emplace_back(std::move(jface));
+          }
+
+          geometry["boundaries"] = {exterior_shell};
+          buildingPart["geometry"].push_back(geometry);
+          outputJSON["CityObjects"][bp_id] = buildingPart;
+        }
+
+        building["children"] = buildingPartIds;
+
+        outputJSON["CityObjects"][b_id] = building;
+      }
+
+      Box bbox;
+      bbox.add(vertex_vec);
+      // auto center = bbox.center();
+      std::vector<std::array<int,3>>vertices_int;
+      for (auto& vertex : vertex_vec) {
+        vertices_int.push_back({ 
+          int( vertex[0] * 1000 ),
+          int( vertex[1] * 1000 ),
+          int( vertex[2] * 1000 )
+        });
+      }
+      outputJSON["vertices"] = vertices_int;
+      outputJSON["transform"] = {
+        {"translate", *manager.data_offset},
+        {"scale", {0.001, 0.001, 0.001}}
+      };
+
+      std::ofstream ofs;
+      ofs.open(manager.substitute_globals(filepath_));
+      ofs << std::fixed << std::setprecision(2);
+      try {
+        ofs << outputJSON.dump(4);
+      } catch (const std::exception& e) {
+        std::cerr << e.what();
+        return;
       }
     }
   };
--- register.hpp
+++ register.hpp
@@ -4,4 +4,5 @@ using namespace geoflow::nodes::cityjson;
 
 void register_nodes(geoflow::NodeRegister& node_register) {
   node_register.register_node<CityJSONReaderNode>("CityJSONReader");
+  node_register.register_node<CityJSONWriterNode>("CityJSONWriter");
 }
