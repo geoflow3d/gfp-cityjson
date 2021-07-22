2965519d1ce349cd327d71b78de9dbe75ff992fa
Ravi Peters <r.y.peters@tudelft.nl>
2021-07-22 16:45:41 +0200

    add attribute writing, multi lod, semantic surfaces

--- nodes.hpp
+++ nodes.hpp
@@ -96,33 +96,90 @@ namespace geoflow::nodes::cityjson {
     
     void init() {
       // declare ouput terminals
-      add_vector_input("multisolids", typeid(std::unordered_map<int, Mesh>));
-      // add_poly_input("attributes", {typeid(bool), typeid(int), typeid(float), typeid(std::string)});
+      add_vector_input("footprints", typeid(LinearRing));
+      add_vector_input("geometry_lod12", typeid(std::unordered_map<int, Mesh>));
+      add_vector_input("geometry_lod13", typeid(std::unordered_map<int, Mesh>));
+      add_vector_input("geometry_lod22", typeid(std::unordered_map<int, Mesh>));
+      add_poly_input("part_attributes", {typeid(bool), typeid(int), typeid(float), typeid(std::string)});
+      add_poly_input("attributes", {typeid(bool), typeid(int), typeid(float), typeid(std::string)});
 
       // declare parameters
       add_param(ParamPath(filepath_, "filepath", "File path"));
       // add_param(ParamInt(extract_lod_, "extract_lod", "precision"));
     }
 
-    void add_vertices(std::map<arr3f, size_t>& vertex_map, std::vector<arr3f>& vertex_vec, std::set<arr3f>& vertex_set, const Mesh& mesh) {
+    void add_vertices_polygon(std::map<arr3f, size_t>& vertex_map, std::vector<arr3f>& vertex_vec, std::set<arr3f>& vertex_set, const LinearRing& polygon) {
       size_t v_cntr = vertex_vec.size();
-      for (auto &face : mesh.get_polygons())
+      for (auto &vertex : polygon)
       {
-        for (auto &vertex : face)
+        auto [it, did_insert] = vertex_set.insert(vertex);
+        if (did_insert)
         {
-          auto [it, did_insert] = vertex_set.insert(vertex);
-          if (did_insert)
-          {
-            vertex_map[vertex] = v_cntr++;
-            vertex_vec.push_back(vertex);
-          }
+          vertex_map[vertex] = v_cntr++;
+          vertex_vec.push_back(vertex);
         }
       }
     }
 
+    void add_vertices_mesh(std::map<arr3f, size_t>& vertex_map, std::vector<arr3f>& vertex_vec, std::set<arr3f>& vertex_set, const Mesh& mesh) {
+      for (auto &face : mesh.get_polygons())
+      {
+        add_vertices_polygon(vertex_map, vertex_vec, vertex_set, face);
+      }
+    }
+
+    std::vector<std::vector<size_t>> LinearRing2jboundary(std::map<arr3f, size_t>& vertex_map, const LinearRing& face) {
+      std::vector<std::vector<size_t>> jface;
+      std::vector<size_t> exterior_ring;
+      for (auto &vertex : face) {
+        exterior_ring.push_back(vertex_map[vertex]);
+      }
+      jface.emplace_back(std::move(exterior_ring));
+      for (auto &iring : face.interior_rings()) {
+        std::vector<size_t> interior_ring;
+        for (auto &vertex : iring) {
+          interior_ring.push_back(vertex_map[vertex]);
+        }
+        jface.emplace_back(std::move(interior_ring));
+      }
+      return jface;
+    }
+
+    nlohmann::json::object_t mesh2jSolid(const Mesh& mesh, const char* lod, std::map<arr3f, size_t>& vertex_map) {
+      auto geometry = nlohmann::json::object();
+      geometry["type"] = "Solid";
+      geometry["lod"] = atof(lod);
+      std::vector<std::vector<std::vector<size_t>>> exterior_shell;
+
+      for (auto &face : mesh.get_polygons())
+      {
+        exterior_shell.emplace_back( LinearRing2jboundary(vertex_map, face) );
+      }
+      geometry["boundaries"] = {exterior_shell};
+
+      auto surfaces = nlohmann::json::array();
+      surfaces.push_back(nlohmann::json::object({{
+        "type", "GroundSurface"
+      }}));
+      surfaces.push_back(nlohmann::json::object({{
+        "type", "RoofSurface"
+      }}));
+      surfaces.push_back(nlohmann::json::object({{
+        "type", "+WallSurface_Outer"
+      }}));
+      surfaces.push_back(nlohmann::json::object({{
+        "type", "+WallSurface_Inner"
+      }}));
+      geometry["semantics"] = {
+        {"surfaces", surfaces},
+        {"values", {mesh.get_labels()}}
+      };
+      return geometry;
+    }
+
     void process() {
       // inputs
-      auto& multisolids = vector_input("multisolids");
+      auto& footprints = vector_input("footprints");
 
       nlohmann::json outputJSON;
 
@@ -135,50 +192,84 @@ namespace geoflow::nodes::cityjson {
       std::set<arr3f> vertex_set;
       size_t id_cntr = 0;
 
-      for (size_t i=0; i<multisolids.size(); ++i) {
-        auto& ms = multisolids.get<std::unordered_map<int, Mesh>>(i);
+      auto& multisolids_lod12 = vector_input("geometry_lod12");
+      auto& multisolids_lod13 = vector_input("geometry_lod13");
+      auto& multisolids_lod22 = vector_input("geometry_lod22");
+      for (size_t i=0; i<multisolids_lod22.size(); ++i) {
 
         auto building = nlohmann::json::object();
         auto b_id = std::to_string(++id_cntr);
         building["type"] = "Building";
-        building["geometry"] = nlohmann::json::array();
         // building["attributes"]
         // building["children"]
 
+        // Building atributes
+        auto jattributes = nlohmann::json::object();
+        for (auto& term : poly_input("attributes").sub_terminals()) {
+          if (!term->get_data_vec()[i].has_value()) continue;
+          auto tname = term->get_name();
+          if (term->accepts_type(typeid(bool))) {
+            jattributes[tname] = term->get<const bool&>(i);
+          } else if (term->accepts_type(typeid(float))) {
+            jattributes[tname] = term->get<const float&>(i);
+          } else if (term->accepts_type(typeid(int))) {
+            jattributes[tname] = term->get<const int&>(i);
+          } else if (term->accepts_type(typeid(std::string))) {
+            jattributes[tname] = term->get<const std::string&>(i);
+          }
+        }
+
+        building["attributes"] = jattributes;
+        
+        // footprint geometry
+        auto fp_geometry = nlohmann::json::object();
+        fp_geometry["lod"] = 0;
+        fp_geometry["type"] = "MultiSurface";
+
+        auto& footprint = footprints.get<LinearRing>(i);
+        add_vertices_polygon(vertex_map, vertex_vec, vertex_set, footprint);
+        fp_geometry["boundaries"] = {LinearRing2jboundary(vertex_map, footprint)};
+        building["geometry"].push_back(fp_geometry);
+        // building["geometry"] = nlohmann::json::array();
+
         std::vector<std::string> buildingPartIds;
 
-        for ( auto& [sid, solid] : ms ) {
+        // geometries
+         const auto& solids_lod12 = multisolids_lod12.get<std::unordered_map<int, Mesh>>(i);
+        const auto& solids_lod13 = multisolids_lod13.get<std::unordered_map<int, Mesh>>(i);
+        for ( const auto& [sid, solid_lod22] : multisolids_lod22.get<std::unordered_map<int, Mesh>>(i) ) {
           auto buildingPart = nlohmann::json::object();
           auto bp_id = std::to_string(++id_cntr);
           buildingPartIds.push_back(bp_id);
           buildingPart["type"] = "BuildingPart";
           buildingPart["parents"] = {b_id};
-          add_vertices(vertex_map, vertex_vec, vertex_set, solid);
-          auto geometry = nlohmann::json::object();
-          geometry["type"] = "Solid";
-          geometry["lod"] = 2.2;
-          std::vector<std::vector<std::vector<size_t>>> exterior_shell;
 
-          for (auto &face : solid.get_polygons())
-          {
-            std::vector<std::vector<size_t>> jface;
-            std::vector<size_t> exterior_ring;
-            for (auto &vertex : face) {
-              exterior_ring.push_back(vertex_map[vertex]);
+         
+          add_vertices_mesh(vertex_map, vertex_vec, vertex_set, solids_lod12.at(sid));
+          add_vertices_mesh(vertex_map, vertex_vec, vertex_set, solids_lod13.at(sid));
+          add_vertices_mesh(vertex_map, vertex_vec, vertex_set, solid_lod22);
+          
+          buildingPart["geometry"].push_back(mesh2jSolid(solids_lod12.at(sid), "1.2", vertex_map));
+          buildingPart["geometry"].push_back(mesh2jSolid(solids_lod13.at(sid), "1.3", vertex_map));
+          buildingPart["geometry"].push_back(mesh2jSolid(solid_lod22, "2.2", vertex_map));
+
+          //attrubutes
+          auto jattributes = nlohmann::json::object();
+          for (auto& term : poly_input("part_attributes").sub_terminals()) {
+            if (!term->get_data_vec()[i].has_value()) continue;
+            auto tname = term->get_name();
+            if (term->accepts_type(typeid(bool))) {
+              jattributes[tname] = term->get<const bool&>(i);
+            } else if (term->accepts_type(typeid(float))) {
+              jattributes[tname] = term->get<const float&>(i);
+            } else if (term->accepts_type(typeid(int))) {
+              jattributes[tname] = term->get<const int&>(i);
+            } else if (term->accepts_type(typeid(std::string))) {
+              jattributes[tname] = term->get<const std::string&>(i);
             }
-            jface.emplace_back(std::move(exterior_ring));
-            for (auto &iring : face.interior_rings()) {
-              std::vector<size_t> interior_ring;
-              for (auto &vertex : iring) {
-                interior_ring.push_back(vertex_map[vertex]);
-              }
-              jface.emplace_back(std::move(interior_ring));
-            }
-            exterior_shell.emplace_back(std::move(jface));
           }
+          buildingPart["attributes"] = jattributes;
 
-          geometry["boundaries"] = {exterior_shell};
-          buildingPart["geometry"].push_back(geometry);
           outputJSON["CityObjects"][bp_id] = buildingPart;
         }
 
